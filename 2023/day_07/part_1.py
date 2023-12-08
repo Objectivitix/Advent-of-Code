7daf050a40cdd3d7e9e82cc06b25be56a617fd16
Objectivitix <79152594+Objectivitix@users.noreply.github.com>
2023-12-07 19:56:34 -0500

    Add 2023 day 7 sol

---
+++ 2023/day_07/part_1.py
@@ -0,0 +1,81 @@
+import functools
+from collections import Counter
+from enum import IntEnum
+
+CARD_VALUES = {
+    "A": 13,
+    "K": 12,
+    "Q": 11,
+    "J": 10,
+    "T": 9,
+    "9": 8,
+    "8": 7,
+    "7": 6,
+    "6": 5,
+    "5": 4,
+    "4": 3,
+    "3": 2,
+    "2": 1,
+}
+
+
+class Tier(IntEnum):
+    FIVE_OF_A_KIND = 6
+    FOUR_OF_A_KIND = 5
+    FULL_HOUSE = 4
+    THREE_OF_A_KIND = 3
+    TWO_PAIR = 2
+    PAIR = 1
+    HIGH_CARD = 0
+
+
+@functools.total_ordering
+class Hand:
+    def __init__(self, cards):
+        self.cards = cards
+        self.cards_n = Counter(cards)
+
+    def __eq__(self, other):
+        return self.strength == other.strength
+
+    def __lt__(self, other):
+        return self.strength < other.strength
+
+    @property
+    def strength(self):
+        return (self.tier, self.order)
+
+    @property
+    def tier(self):
+        match [n for _, n in self.cards_n.most_common(2)]:
+            case [5]:
+                return Tier.FIVE_OF_A_KIND
+            case [4, _]:
+                return Tier.FOUR_OF_A_KIND
+            case [3, 2]:
+                return Tier.FULL_HOUSE
+            case [3, 1]:
+                return Tier.THREE_OF_A_KIND
+            case [2, 2]:
+                return Tier.TWO_PAIR
+            case [2, 1]:
+                return Tier.PAIR
+            case _:
+                return Tier.HIGH_CARD
+
+    @property
+    def order(self):
+        return tuple(CARD_VALUES[card] for card in self.cards)
+
+
+with open("input.txt") as file:
+    inp = file.read().splitlines()
+
+hands = []
+
+for line in inp:
+    hand_raw, bid_raw = line.split()
+
+    hands.append((Hand(hand_raw), int(bid_raw)))
+
+print(sum(bid * rank for rank, (_, bid) in enumerate(sorted(hands), start=1)))
---
+++ 2023/day_07/part_2.py
@@ -0,0 +1,91 @@
+import functools
+from collections import Counter
+from enum import IntEnum
+
+JOKER = "J"
+
+CARD_VALUES = {
+    "A": 13,
+    "K": 12,
+    "Q": 11,
+    "T": 10,
+    "9": 9,
+    "8": 8,
+    "7": 7,
+    "6": 6,
+    "5": 5,
+    "4": 4,
+    "3": 3,
+    "2": 2,
+    "J": 1,
+}
+
+
+class Tier(IntEnum):
+    FIVE_OF_A_KIND = 6
+    FOUR_OF_A_KIND = 5
+    FULL_HOUSE = 4
+    THREE_OF_A_KIND = 3
+    TWO_PAIR = 2
+    PAIR = 1
+    HIGH_CARD = 0
+
+
+@functools.total_ordering
+class Hand:
+    def __init__(self, cards):
+        self.cards = cards
+
+        self.jokers_n = cards.count(JOKER)
+        self.cards_n = Counter(cards.replace(JOKER, ""))
+
+    def __eq__(self, other):
+        return self.strength == other.strength
+
+    def __lt__(self, other):
+        return self.strength < other.strength
+
+    @property
+    def strength(self):
+        return (self.tier, self.order)
+
+    @property
+    def tier(self):
+        if self.jokers_n == 5:
+            return Tier.FIVE_OF_A_KIND
+
+        indicators = [n for _, n in self.cards_n.most_common(2)]
+        indicators[0] += self.jokers_n
+
+        match indicators:
+            case [5]:
+                return Tier.FIVE_OF_A_KIND
+            case [4, *_]:
+                return Tier.FOUR_OF_A_KIND
+            case [3, 2]:
+                return Tier.FULL_HOUSE
+            case [3, 1]:
+                return Tier.THREE_OF_A_KIND
+            case [2, 2]:
+                return Tier.TWO_PAIR
+            case [2, 1]:
+                return Tier.PAIR
+            case _:
+                return Tier.HIGH_CARD
+
+    @property
+    def order(self):
+        return tuple(CARD_VALUES[card] for card in self.cards)
+
+
+with open("input.txt") as file:
+    inp = file.read().splitlines()
+
+hands = []
+
+for line in inp:
+    hand_raw, bid_raw = line.split()
+
+    hands.append((Hand(hand_raw), int(bid_raw)))
+
+print(sum(bid * rank for rank, (_, bid) in enumerate(sorted(hands), start=1)))
