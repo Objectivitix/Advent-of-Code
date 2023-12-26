b7230d5b9c4ce4d24e49db2d1f03614ba82e4445
Objectivitix <79152594+Objectivitix@users.noreply.github.com>
2023-12-25 19:59:58 -0500

    Add 2023 day 25 solution

---
+++ 2023/day_25/part_1.py
@@ -0,0 +1,93 @@
+# Karger's algorithm, a randomized algo for finding
+# the global minimum cut in an undirected, unweighted
+# graph. With |V| choose 2 trials, the probability
+# of NOT finding the min cut is about 1 over |V|.
+#
+# I'm not sure if I implemented the algo right,
+# because my solution is WAY too slow to run even |V|
+# trials, let alone |V| choose 2 trials. So since we
+# know the min cut size is 3 ... I "bogosort" it :P
+#
+# Better solution in a separate file, coming soon.
+
+import random
+from collections import defaultdict
+from copy import deepcopy
+
+
+class Graph:
+    def __init__(self):
+        self.adj_list = defaultdict(list)
+
+    def add_edge(self, u, v):
+        self.adj_list[u].append(v)
+        self.adj_list[v].append(u)
+
+    def _contract_edge(self, u, v):
+        u_adjs = self.cut[u]
+        v_adjs = self.cut[v]
+
+        del self.cut[u]
+        del self.cut[v]
+
+        proto = []
+
+        for node in (u, v):
+            if isinstance(node, frozenset):
+                proto.extend(node)
+            else:
+                proto.append(node)
+
+        supernode = frozenset(proto)
+
+        self.cut[supernode] = [
+            neighbor
+            for neighbor in [*u_adjs, *v_adjs]
+            if neighbor not in (u, v)
+        ]
+
+        for neighbor in self.cut[supernode]:
+            self.cut[neighbor] = [
+                supernode if node in (u, v) else node
+                for node in self.cut[neighbor]
+            ]
+
+    def sample_karger_cut(self):
+        self.cut = deepcopy(self.adj_list)
+
+        while len(self.cut) > 2:
+            u = random.choice(list(self.cut))
+            v = random.choice(self.cut[u])
+
+            self._contract_edge(u, v)
+
+        s, t = self.cut
+
+        return s, t, len(self.cut[s])
+
+
+def parse_raw(file):
+    apparatus = Graph()
+    components = set()
+
+    for line in file.read().splitlines():
+        component, neighbors = line.split(": ")
+        components.add(component)
+
+        for neighbor in neighbors.split(" "):
+            apparatus.add_edge(component, neighbor)
+            components.add(neighbor)
+
+    return apparatus, len(components)
+
+
+with open("input.txt") as file:
+    APPARATUS, NODES_N = parse_raw(file)
+
+while True:
+    s, t, size = APPARATUS.sample_karger_cut()
+
+    if size == 3:
+        break
+
+print(len(s) * len(t))
